68bac38d1eec10049a076f7ebcbfee6a132966e5
AhmadNasser04 <ahmad_nasser04@outlook.com>
2023-01-01 22:57:00 +0200

    oof

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import PublicIcon from "@mui/icons-material/Public";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -46,18 +47,10 @@ function App() {
       <footer className="sticky bottom-5 w-full cursor-pointer">
         <a href="#hero" alt="Home" title="Goes up to hero section">
           <div className="flex items-center justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="#F7AB0A"
-              className="w-8 h-8 filter grayscale hover:grayscale-0 transition duration-150"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm.53 5.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72v5.69a.75.75 0 001.5 0v-5.69l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <PublicIcon
+              fontSize="large"
+              className="text-[#F7AB0A] filter grayscale hover:grayscale-0 transition duration-150"
+            />
           </div>
         </a>
       </footer>
